b844c6523bb769f6c3ed2dc4bd3e9b856c006612
Ricardo Barbosa <dotnetricardo@hotmail.com>
2015-01-17 10:19:53 +0000

    This is a test
    test

---
+++ TestPlugin.cs
@@ -0,0 +1,17 @@
+﻿using Shape.Contracts;
+using Shape.Core;
+
+namespace Plugin
+{
+    using System.Threading.Tasks;
+
+    [Shape("TestPlugin", false)]
+    public class TestPlugin
+    {
+        public async Task<string> Message(string message)
+        {
+            return await Task.FromResult(message);
+        }
+
+    }
+}
